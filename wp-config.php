7d47dd3160572c14dd75377bcf8728f7b6284611
Samrat Khan <takeitkhan@gmail.com>
2026-01-11 22:15:09 +0600

    mm-spg backup removed from the root directory and db added

--- wp-config.php
+++ wp-config.php
@@ -22,16 +22,10 @@ error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'pet' );
-
-/** Database username */
-define( 'DB_USER', 'root' );
-
-/** Database password */
-define( 'DB_PASSWORD', '' );
-
-/** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define('DB_NAME', 'wp');
+define('DB_USER', 'wp');
+define('DB_PASSWORD', 'wp');
+define('DB_HOST', 'db'); // NOT localhost
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
